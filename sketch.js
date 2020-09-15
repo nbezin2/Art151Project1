db99a272cc9220baa599fec225d3e4a3d1d087c8
nbezin2 <47515136+nbezin2@users.noreply.github.com>
2020-09-14 23:12:30 -0500

    update again
    making window smaller adjusting values
    I think that 2 "Brushes" running makes the most interesting designs

--- sketch.js
+++ sketch.js
@@ -1,25 +1,19 @@
 let allBrush = [];
-let allDX = [3,5];
-let backC = 0;
 
 function setup() {
-	// put setup code here
-	backC = map(mouseX+mouseY, 0,800, 0,255);
-	
-	createCanvas(1920,700);
-	background(backC);
+	createCanvas(500,500);
+	background(0);
 	
 	for (let i = 0; i < 2; i++) {
-		allBrush.push(new Brush(5, random(0,80)*5, 5, 5, random(0,256), random(0,256), random(0,256), allDX[i], allDX[i]/5));
+		allBrush.push(new Brush(random(20,450), random(5,450), 5, 5, random(0,256), random(0,256), random(0,256), random(1,10), random(1,10)));
+		
 	}
-	
 }
 
 function draw() {
-	//put drawing code here
-	for (let i=0; i<2; i++) {
+	for (let i=0; i<2;i++) {
 		allBrush[i].show();
-		allBrush[i].updatePos();
+		allBrush[i].update();
 	}
 }
 
